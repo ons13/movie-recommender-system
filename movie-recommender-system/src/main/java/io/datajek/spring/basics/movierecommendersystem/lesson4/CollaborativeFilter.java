5f609401df8e769686685e5e0b671602e9a7d024
ons13 <onsbhs3@gmail.com>
2023-11-22 14:55:54 +0100

    Lesson4

--- movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson4/CollaborativeFilter.java
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson4/CollaborativeFilter.java
@@ -1,8 +1,6 @@
-package io.datajek.spring.basics.movierecommendersystem.lesson3;
+package io.datajek.spring.basics.movierecommendersystem.lesson4;
 
 import io.datajek.spring.basics.movierecommendersystem.lesson2.Filter;
-import org.springframework.stereotype.Component;
-@Component
 
 public class CollaborativeFilter implements Filter {
     public String[] getRecommendations(String movie){
