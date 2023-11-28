77c8f1124c6102b657d48b4052d60d137c2e55f7
ons13 <onsbhs3@gmail.com>
2023-11-28 17:51:38 +0100

    Lesson9 Proxy / Mixing Bean Scope

---
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson9/ContentBasedFilter.java
@@ -0,0 +1,36 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson9;
+
+import io.datajek.spring.basics.movierecommendersystem.lesson2.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ContentBasedFilter implements Filter{
+
+    private static int instances= 0;
+
+    @Autowired
+    private Movie movie;
+
+    public ContentBasedFilter() {
+        instances++;
+        System.out.println("ContentBasedFilter constructor called");
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public static int getInstances(){
+        return ContentBasedFilter.instances;
+    }
+
+
+    //getRecommendations takes a movie as input and returns a list of similar movies
+    public String[] getRecommendations(String movie) {
+        //calculate similarity between movies
+        //return movie recommendations
+        return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
+    }
+}
